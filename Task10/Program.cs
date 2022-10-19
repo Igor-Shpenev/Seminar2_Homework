638affbe3892e76cee55a22486191d94c534f8ab
Igor Shpenev <vdvshneek@yandex.ru>
2022-10-19 15:37:20 +0500

    Completed task number ten

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int SecondDigit(int number)
+{
+    int digit23 = number % 100;
+    int digit2 = digit23 / 10;
+    return digit2;
+
+}
+
+int randNum = new Random().Next(100, 1000);
+int result = SecondDigit(randNum);
+Console.WriteLine($"Second digit of number {randNum} is {result}");
